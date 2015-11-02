6b1b5393f2f0d60b6866f87c2e060f76e0f55f56
gr4viton <lordmutty@gmail.com>
2015-11-02 16:28:15 +0100

    haydee hoooo

--- kivyCV.py
+++ kivyCV.py
@@ -59,15 +59,8 @@ class Multicopter(GridLayout):
         # make sure we aren't overriding any important functionality
         super(Multicopter, self).__init__(**kwargs)
         self.capture_control = capture_control
-        # self.root_toggle_findtag = findtag_control.find_tag
-        self.root_toggle_findtag = None
+        self.findtag_control = findtag_control
 
-    def toggle_findtag(self):
-        self.root_toggle_findtag()
-
-
-    def toggle_source_id(self):
-        self.capture_control.toggle_source_id()
 
     def sla_steps_add_widgets(self,imList):
         diff = len(imList) - len(self.sla_steps.children)
@@ -174,37 +167,91 @@ class LockedNumpyArray(object):
         self.val = val.copy() # ????????????????????????????????????????? do i need it?
         self.lock.release()
 
-class FindTagControl():
+class FindtagControl():
     """
     Shared class to control findtag algorythm execution
     """
 
-    def __init__(self, lock):
-        self.cTag = None
-        self.imSteps = None
-        self.imTags = None
-        self.findtag_running = False
+    im_steps = LockedNumpyArray()
+    im_tags = LockedNumpyArray()
+    findtagging = LockedValue(False)
+    cTag = LockedValue()
+
+    def __init__(self, capture_control):
 
 
-class FindTagThread(threading.Thread):
-    """
-    Thread running find tag algorythm
-    """
-    def __init__(self, findtag_control, capture_control):
-        threading.Thread.__init__(self)
-        self.findtag_control = findtag_control
         self.capture_control = capture_control
+        # self.capture_lock = threading.Lock()
+        # self.capture = None
+        # self.source_id = 0
 
-    def run(self):
-        while True:
-            pass
-            # frame = self.capture_control.get_frame()
-            # imSteps, imTags = stepCV(frame,cTag)
-            # self.findtag_control.imTags = imTags.copy()
-            # self.findtag_control.imSteps = imSteps.copy()
-            #do the math
-            # copy
-            # findtag_control
+        self.init_findtag()
+
+        self.sleepTime = 0.0
+
+    def init_findtag(self):
+        # load cTag
+        pass
+    #     self.capture_lock.acquire()
+    #     self.capture = cv2.VideoCapture()
+    #     self.capture_lock.release()
+    #
+    # def open_source_id(self, new_source_id):
+    #     self.capture_lock.acquire()
+    #     self.source_id = new_source_id
+    #     self.capture_lock.release()
+    #     self.open_source_id()
+    #
+    # def open_capture(self):
+    #     self.capture_lock.acquire()
+    #     self.capture.open(self.source_id)
+    #     if self.capture.isOpened() != True:
+    #         raise('Cannot open capture source_id ', self.source_id)
+    #     print('Opened capture source_id ' + str(self.source_id))
+    #     self.capture_lock.release()
+    #
+    # def toggle_source_id(self):
+    #     self.capture_lock.acquire()
+    #     self.source_id = np.mod(self.source_id+1, 2)
+    #     self.capture_lock.release()
+    #     self.open_capture()
+    #
+    # def close_capture(self):
+    #     self.capture_lock.acquire()
+    #     self.capture.release()
+    #     self.capture_lock.release()
+    #
+    def start_findtagging(self):
+        self.findtagging = True
+        self.thread = threading.Thread(target=self.findtag_loop)
+        self.thread.start()
+
+    def toggle_findtagging(self):
+        if self.findtagging == False:
+            self.start_findtagging()
+        else:
+            self.findtagging = False
+
+    def on_stop(self):
+        self.findtagging = False
+
+    def findtag_loop(self):
+        while self.findtagging:
+            self.findtag()
+
+    def findtag(self):
+
+        imGray, imTags = stepCV(self.capture_control.frame, self.cTag)
+        # self.capture_lock.acquire()
+        # if self.capture.isOpened() != True:
+        #     # self.open_capture()
+        #     raise('Cannot read frame as the capture is not opened')
+        # else:
+        #     ret, frame = self.capture.read()
+        # self.capture_lock.release()
+        # self.frame = frame
+        # # print(self.frame.shape)
+
 
 
 
@@ -216,31 +263,21 @@ class CaptureControl():
     """
     frame = LockedNumpyArray( np.ones( (32,24,3,), np.uint8 ) * 255 )
     capturing = LockedValue(False)
-    thread_running = LockedValue(False)
 
     def __init__(self):
         self.capture_lock = threading.Lock()
         self.capture = None
-        self.source_id = 0
+
         self.init_capture()
-        self.sleepTime = 0.01
-        self.thread = threading.Thread(target=self.capture_loop)
+
+        self.source_id = 0
+
+        self.sleepTime = 0.0
 
     def init_capture(self):
         self.capture_lock.acquire()
         self.capture = cv2.VideoCapture()
         self.capture_lock.release()
-
-    def capture_frame(self):
-        self.capture_lock.acquire()
-        if self.capture.isOpened() != True:
-            # self.open_capture()
-            raise('Cannot read frame as the capture is not opened')
-        else:
-            ret, frame = self.capture.read()
-        self.capture_lock.release()
-        self.frame = frame
-        print(self.frame.shape)
 
     def open_source_id(self, new_source_id):
         self.capture_lock.acquire()
@@ -270,23 +307,35 @@ class CaptureControl():
     def start_capturing(self):
         self.open_capture()
         self.capturing = True
-        self.thread_running = True
+        self.thread = threading.Thread(target=self.capture_loop)
         self.thread.start()
 
+    def toggle_capturing(self):
+        if self.capturing == False:
+            self.start_capturing()
+        else:
+            self.capturing = False
+
     def on_stop(self):
+        # stops capturing and releases capture object
         self.capturing = False
-        self.thread_running = False
         self.close_capture()
 
     def capture_loop(self):
-        while self.thread_running:
-            if self.capturing:
-                self.capture_frame()
-            # print(time.time(),' - ', threading.current_thread().name)
-            # threading.currentThread.
-            time.sleep(self.sleepTime)
-            # frames?
+        while self.capturing:
+            self.capture_frame()
+            time.sleep(self.sleepTime)  # frames?
 
+    def capture_frame(self):
+        self.capture_lock.acquire()
+        if self.capture.isOpened() != True:
+            # self.open_capture()
+            raise('Cannot read frame as the capture is not opened')
+        else:
+            ret, frame = self.capture.read()
+        self.capture_lock.release()
+        self.frame = frame
+        # print(self.frame.shape)
 
 
 
@@ -310,7 +359,11 @@ class multicopterApp(App):
 
         self.capture_control = CaptureControl()
         self.capture_control.start_capturing()
-        self.root = root = Multicopter(self.capture_control, None)
+
+        self.findtag_control = FindtagControl(self.capture_control)
+        self.findtag_control.start_findtagging
+
+        self.root = root = Multicopter(self.capture_control, self.findtag_control)
         self.build_opencv()
         return root
 
@@ -337,58 +390,39 @@ class multicopterApp(App):
         # self.set_running_findtag(True)
 
 
-    # def set_running_findtag(self, new_value):
-    #     if self.running_findtag != new_value:
-    #         # Clock.schedule_interval(self.update_findtag, self.fps_findTag )
-    #         self.running_findtag = new_value
 
-    # def start_findtag(self):
-    #     if self.running_findtag == False:
-    #         # Clock.schedule_interval(self.update_findtag, self.fps_findTag )
-    #         self.running_findtag = True
-    #         print('Started findtag algorythm')
-    #
-    # def stop_findtag(self):
-    #     if self.running_findtag == True:
-    #         # Clock.unschedule(self.update_findtag)
-    #         self.running_findtag = False
-    #         print('Stopped findtag algorythm')
+    def update_findtag_gui(frame, cTag, running_findtag):
 
-    # def toggle_findtag(self):
-    #     self.set_running_findtag(not self.running_findtag)
+        while True:
+            if running_findtag:
 
-    # def update_findtag(frame, cTag, running_findtag):
-    #
-    #     while True:
-    #         if running_findtag:
-    #
-    #             start = timeit.timeit()
-    #             # imList, imTags = stepCV(frame,self.cTag)
-    #             imGray, imTags = stepCV(frame, cTag)
-    #             # time.sleep(1)
-    #             end = timeit.timeit()
-    #             # print(end - start)
-    #
-    #             # self.root.img_webcam.texture = self.convert_to_texture(frame)
-    #             # imList = imList.copy()
-    #             # self.root.update_sla_steps(imList)
-    #
-    #
-    #             # if len(self.root.sla_steps.children) == 0:
-    #             #     self.root.sla_steps.add_widget(Image())
-    #             # self.root.sla_steps.children[0].texture = convert_to_texture(imGray)
-    #
-    #             # if imTags is not None:
-    #             #     # self.root.txt_numFound.text = str(len(imTags))
-    #             #     if len(imTags) > 0:
-    #             #         imAllTags = fh.joinIm( [[im] for im in imTags], 1 )
-    #             #         # update_image(image_label, imAllTags)
-    #             #         if len(imAllTags.shape) == 2:
-    #             #             imAllTags = cv2.cvtColor(imAllTags, cv2.COLOR_GRAY2RGB)
-    #             #         print(imAllTags)
-    #             #         print(imAllTags.shape)
-    #             #         self.root.img_tags.texture = self.convert_to_texture( imAllTags.copy() )
-    #
+                start = timeit.timeit()
+                # imList, imTags = stepCV(frame,self.cTag)
+                imGray, imTags = stepCV(frame, cTag)
+                # time.sleep(1)
+                end = timeit.timeit()
+                # print(end - start)
+
+                # self.root.img_webcam.texture = self.convert_to_texture(frame)
+                # imList = imList.copy()
+                # self.root.update_sla_steps(imList)
+
+
+                # if len(self.root.sla_steps.children) == 0:
+                #     self.root.sla_steps.add_widget(Image())
+                # self.root.sla_steps.children[0].texture = convert_to_texture(imGray)
+
+                # if imTags is not None:
+                #     # self.root.txt_numFound.text = str(len(imTags))
+                #     if len(imTags) > 0:
+                #         imAllTags = fh.joinIm( [[im] for im in imTags], 1 )
+                #         # update_image(image_label, imAllTags)
+                #         if len(imAllTags.shape) == 2:
+                #             imAllTags = cv2.cvtColor(imAllTags, cv2.COLOR_GRAY2RGB)
+                #         print(imAllTags)
+                #         print(imAllTags.shape)
+                #         self.root.img_tags.texture = self.convert_to_texture( imAllTags.copy() )
+
 
 
 
