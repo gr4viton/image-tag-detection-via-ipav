ef023d5006ccb39bbedc42d958b17da399a83e4e
gr4viton <lordmutty@gmail.com>
2015-12-02 12:52:07 +0100

    st 02.12.2015 12:52:07,27

--- _gr4_git_update.bat
+++ _gr4_git_update.bat
@@ -1,8 +1,21 @@
 D:
 cd D:\DEV\PYTHON\pyCV\2015_10_11 - pycmulti\
+
+echo Current working directory is %cd%
+echo Changing direcotory to %cd%
+
 git.exe add .
 git.exe commit -m "%date% %time%"
 git.exe pull -v --no-rebase --progress "origin"
 git.exe commit -m "%date% %time% after pull"
 git.exe push origin master
+
 python.exe -c "print('\7')" :: BEEP
+
+set /a time2wait=%1	:: from first parameter
+echo %time2wait%
+if %time2wait% EQU 0 (
+	timeout 3
+) ELSE (
+	timeout %time2wait%
+)
